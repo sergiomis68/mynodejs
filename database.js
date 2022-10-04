74ef2f7a0d47196df7a6b750c60d27ed52c13bdf
sergiomis68 <113993121+sergiomis68@users.noreply.github.com>
2022-10-04 08:40:36 +0200

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var conn = mysql.createConnection({
   host: 'localhost', // Replace with your host name
-  user: 'root',      // Replace with your database username
-  password: '',      // Replace with your database password
-  database: 'nodeapp' // // Replace with your database Name
+  user: 'tester',      // Replace with your database username
+  password: 'Pass1234',      // Replace with your database password
+  database: 'testdb' // // Replace with your database Name
 }); 
 conn.connect(function(err) {
   if (err) throw err;
